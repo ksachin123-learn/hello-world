32ecc70b30a4ae3588d773bb4b95bc4a42210c01
Sachin Kalsekar <ksachin123@yahoo.com>
2021-04-28 18:02:33 +0530

    Updated greetings. AB#1

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot with azure board!";
+		return "Greetings from Spring Boot with azure board!!!";
 	}
 }
